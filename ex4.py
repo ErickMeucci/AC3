2006aa806335bf3c25eb3f5f1fd49aaf5f068d79
ErickMeucci <144945118+ErickMeucci@users.noreply.github.com>
2023-10-10 14:12:46 -0300

    Ajustando
    Trocando o programa pra um que possua o while

--- ex4.py
+++ ex4.py
@@ -9,12 +9,18 @@ def eh_bissexto(ano):
 ano_inicio = int(input())
 ano_fim = int(input())
 
-# Loop para encontrar e exibir os anos bissextos no intervalo
+# Inicializa o ano atual com o ano de início
+ano_atual = ano_inicio
+
+# Inicializa o contador de anos bissextos
 contador_bissextos = 0
-for ano in range(ano_inicio, ano_fim + 1):
-    if eh_bissexto(ano):
-        print(ano)
+
+# Loop while para encontrar e exibir os anos bissextos no intervalo
+while ano_atual <= ano_fim:
+    if eh_bissexto(ano_atual):
+        print(ano_atual)
         contador_bissextos += 1
+    ano_atual += 1
 
 # Exibição da quantidade de anos bissextos no intervalo
-print(f"bissexto: {contador_bissextos}")
+print(f"bissextos: {contador_bissextos}")
